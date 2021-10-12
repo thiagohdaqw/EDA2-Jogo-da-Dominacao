37901fbaf590376b777ad1ae8addf4d5404e2924
Yudi Yamane <yudiyamane@gmail.com>
2021-10-12 18:09:00 -0300

    run.sh runs only, does not build

--- run.sh
+++ run.sh
@@ -1,17 +1,10 @@
+#!/bin/bash
+
 ARB_ARGS="$*";
 BIN_PATH="./bin";
 MENSAGENS="$BIN_PATH/mensagens";
 
-if [ ! -d $BIN_PATH ]; then
-	mkdir $BIN_PATH;
-fi
-
-if [ ! -e "$MENSAGENS" ]; then
-	mkfifo $MENSAGENS;
-fi
-
-gcc -o "$BIN_PATH/arbitro" arbitro.c -g -lm
-gcc -o "$BIN_PATH/jogador" jogador.c -DDEBUG=1 -g
-
 $BIN_PATH/arbitro $ARB_ARGS < $MENSAGENS | $BIN_PATH/jogador > $MENSAGENS
 #$BIN_PATH/arbitro $ARB_ARGS < $MENSAGENS | valgrind $BIN_PATH/jogador > $MENSAGENS
+
+exit
